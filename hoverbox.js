26c443118b26cc42a0b19f5dc4537538993fb089
kernelly <kernelly0206@gmail.com>
2026-02-17 22:00:07 +0000

    Fixing

--- hoverbox.js
+++ hoverbox.js
@@ -1,16 +1,16 @@
-const box = document.getElementByClass('expandable');
+document.querySelectorAll('.expandable').forEach(box => {
+  const originalWidth = box.offsetWidth;
+  const originalHeight = box.offsetHeight;
 
-const originalWidth = box.offsetWidth;
-const originalHeight = box.offsetHeight;
-
-box.addEventListener('mouseover', () => {
+  box.addEventListener('mouseover', () => {
     box.style.width = originalWidth * 1.5 + 'px';
     box.style.height = originalHeight * 1.5 + 'px';
-    box.style.backgroundColor = 'tomato'; // Optional color change
-});
+    box.style.backgroundColor = 'tomato';
+  });
 
-box.addEventListener('mouseout', () => {
+  box.addEventListener('mouseout', () => {
     box.style.width = originalWidth + 'px';
     box.style.height = originalHeight + 'px';
-    box.style.backgroundColor = 'steelblue';
+    box.style.backgroundColor = '';
+  });
 });
